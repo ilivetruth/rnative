35c47db08d1fa9d16ae9b66671c78961575e7a2b
iLiveTruth <122330981+ilivetruth@users.noreply.github.com>
2024-08-24 20:46:46 -0400

    update app/_layout.tsx

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -2,8 +2,17 @@ import { Stack } from "expo-router";
 // testing
 export default function RootLayout() {
   return (
-    <Stack>
-      <Stack.Screen name="index" />
+    <Stack
+      screenOptions={{
+        headerStyle: {
+          backgroundColor: '#0062fe',
+        },
+        headerTintColor: '##ffffff',
+        headerTitleStyle: {
+          fontWeight: 'bold',
+        },
+      }}>
+      <Stack.Screen name="index"  />
     </Stack>
   );
 }
