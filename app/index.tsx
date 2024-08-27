7a53859416b665db8a52e71c6b1d3418f93eda59
iLiveTruth <122330981+ilivetruth@users.noreply.github.com>
2024-08-27 18:12:03 -0400

    update app/index.tsx

--- app/index.tsx
+++ app/index.tsx
@@ -11,7 +11,7 @@ export default function Index() {
         alignItems: "center",
       }}
     >
-      <View style={{styles.container}}>
+      <View style={styles.container}>
         
       </View>
     </ThemedView>
